64312632497d2ddb5fe922e481e30f525ec60ac2
ldulibic <lovro.dulibic@gmail.com>
2021-10-01 14:32:49 +0200

    ..

--- higgsTest_GEN.py
+++ higgsTest_GEN.py
@@ -65,26 +65,27 @@ if options.withNu:
     jetLabel = ("ak8GenJets")
 
 # loop over events
-for i,event in enumerate(events): 
-    event.getByLabel(gpLabel, gpHandle)
-    genparticles = gpHandle.product()
-    event.getByLabel(jetLabel, jetHandle)
-    jets = jetHandle.product()
+#for i,event in enumerate(events): 
+event = events[2]
 
-    noDaughters = 0
-    higgsList=[]
-    higgscount=0
-    for gp in genparticles:
-        if not gp.pdgId()==25:
-            continue
-        hasHiggsDaughter = False
-        for d in range(gp.numberOfDaughters()):
-            if gp.daughter(d).pdgId()==25:
-                hasHiggsDaughter = True
-                break
-        if hasHiggsDaughter:
-            noDaughters += 1
-            continue
-    break
+event.getByLabel(gpLabel, gpHandle)
+genparticles = gpHandle.product()
+event.getByLabel(jetLabel, jetHandle)
+jets = jetHandle.product()
+
+noDaughters = 0
+higgsList=[]
+higgscount=0
+for gp in genparticles:
+    if not gp.pdgId()==25:
+        continue
+    hasHiggsDaughter = False
+    for d in range(gp.numberOfDaughters()):
+        if gp.daughter(d).pdgId()==25:
+            hasHiggsDaughter = True
+            break
+    if hasHiggsDaughter:
+        noDaughters += 1
+        continue
 
 print(noDaughters)
